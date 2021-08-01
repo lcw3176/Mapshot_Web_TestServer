b934e96051dbe337e4935b15b3cd01d16d086d42
lcw3176 <lcw3176@naver.com>
2021-08-01 17:20:30 +0900

    test

--- js/app.js
+++ js/app.js
@@ -58,7 +58,7 @@ window.onload = function(){
             document.getElementById("showIfLayerOnly").style.display = "none";
         }
 
-        setLayersFormat(0);
+        document.getElementById("default_click_format").click();
     }
 
     document.getElementById("default_click_level").click();
