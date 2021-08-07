d607cd5d69c57f54b16cfb108bca379a469878c4
joebrooks <lcw3176@naver.com>
2021-08-07 21:41:06 +0900

    test

--- preview/static/js/1.0.0/maps/tile.js
+++ preview/static/js/1.0.0/maps/tile.js
@@ -17,14 +17,14 @@ class Tile{
 
 
     getSW(sideBlockCount, NFixLat, coor){
-        var Lat = coor.getY() - NFixLat.getWidthBetweenBlock() * parseInt(sideBlockCount / 2) - NFixLat.getHeightBetweenBlock() / 2;
+        var Lat = coor.getY() - NFixLat.getWidthBetweenBlock() * parseInt(sideBlockCount / 2) - NFixLat.getWidthBetweenBlock() / 2;
         var Lng = coor.getX() - NFixLat.getHeightBetweenBlock() * parseInt(sideBlockCount / 2) - NFixLat.getHeightBetweenBlock() / 2;
 
         return new mapshot.coors.LatLng(Lat, Lng);
     }
 
     getNE(sideBlockCount, NFixLat, coor){
-        var Lat = coor.getY() + NFixLat.getWidthBetweenBlock() * parseInt(sideBlockCount / 2) + NFixLat.getHeightBetweenBlock() / 2;
+        var Lat = coor.getY() + NFixLat.getWidthBetweenBlock() * parseInt(sideBlockCount / 2) + NFixLat.getWidthBetweenBlock() / 2;
         var Lng = coor.getX() + NFixLat.getHeightBetweenBlock() * parseInt(sideBlockCount / 2) + NFixLat.getHeightBetweenBlock() / 2;
 
         return new mapshot.coors.LatLng(Lat, Lng);
