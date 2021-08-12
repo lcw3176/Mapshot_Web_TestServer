41406b335b332dc2bfa2b4c0db7db57fc463924f
lcw3176 <lcw3176@naver.com>
2021-08-13 00:44:47 +0900

    test

--- preview/static/js/app.js
+++ preview/static/js/app.js
@@ -155,13 +155,14 @@ window.onload = function () {
     }
 
     kakaoCapture = function(){
-        var requestUrl = "https://mapshotproxyserver.herokuapp.com/main?";
+        var requestUrl = "https://mapshotproxyserver.herokuapp.com/test?";
         var queryString = "lat=" + coor.getY() + "&lng=" + coor.getX() + "&level=" + km;
         
         requestUrl += queryString;
         
         var img = new Image();
         img.src = requestUrl;
+        img.crossOrigin = "*";
         document.getElementById("captureStatus").innerText = "서버에 요청중입니다. 잠시 기다려주세요";
         
         img.onload = function(){
