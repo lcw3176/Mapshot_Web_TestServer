a9dc242d6a70d3f95e8c359e6ad6b844806ad902
joebrooks <lcw3176@naver.com>
2022-01-06 14:34:17 +0900

    navbar 로딩 스크립트

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -1,12 +1,15 @@
 window.addEventListener("load", function(){
-    navbarBurgerClick = function(id){
-        if(id.getAttribute("class") === "navbar-burger"){
-            id.setAttribute("class", "navbar-burger is-active");
-            document.getElementById("navbar-menu").setAttribute("class", "navbar-menu is-active");
-        } else{
-            id.setAttribute("class", "navbar-burger");
-            document.getElementById("navbar-menu").setAttribute("class", "navbar-menu");
+    var xml = new XMLHttpRequest();
+    xml.onreadystatechange = function(){
+
+        if(this.status == 200 && this.readyState == this.DONE) {
+            document.getElementById("navbar").innerHTML = xml.responseText;            
         }
     }
+
+    xml.open("GET", "../static/template/navbar.html", true);
+    xml.setRequestHeader("Access-Control-Allow-Origin", "*");
+    xml.send();
+
 })
 
